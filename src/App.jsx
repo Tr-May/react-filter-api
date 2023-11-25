6a1d1f754807fba37e212e79c06b03aa6549c8a0
Tr-May <may.passionateteacher@gmail.com>
2023-11-25 11:35:03 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      // const response = await axios.get("https://fakestoreapi.com/products");
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      // console.log(response.data);
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container my-3 bg-light">
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search by Name"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <table className="table table-striped mt-3">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.name.toLowerCase().includes(search);
+            })
+            .map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.website}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    id: 1,
+    name: "Reggie",
+    email: "rkirley0@google.ru",
+    gender: "Female",
+  },
+  {
+    id: 2,
+    name: "Ignatius",
+    email: "ileward1@wikia.com",
+    gender: "Male",
+  },
+  {
+    id: 3,
+    name: "Debby",
+    email: "dburghall2@dell.com",
+    gender: "Female",
+  },
+  {
+    id: 4,
+    name: "Dukie",
+    email: "dwoodeson3@imdb.com",
+    gender: "Male",
+  },
+  {
+    id: 5,
+    name: "Rochette",
+    email: "ryanov4@mediafire.com",
+    gender: "Female",
+  },
+  {
+    id: 6,
+    name: "Rocky",
+    email: "rarp5@state.gov",
+    gender: "Bigender",
+  },
+  {
+    id: 7,
+    name: "Tarra",
+    email: "teden6@behance.net",
+    gender: "Female",
+  },
+  {
+    id: 8,
+    name: "Deane",
+    email: "dscoates7@newyorker.com",
+    gender: "Female",
+  },
+  {
+    id: 9,
+    name: "Wallie",
+    email: "wiwanczyk8@quantcast.com",
+    gender: "Male",
+  },
+  {
+    id: 10,
+    name: "Konstance",
+    email: "khanniger9@bizjournals.com",
+    gender: "Female",
+  },
+];
