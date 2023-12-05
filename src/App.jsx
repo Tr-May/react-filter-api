cd438c5d1d5300c3d2a142622ff5c19cbd3a692e
Tr-May <may.passionateteacher@gmail.com>
2023-12-05 14:09:24 +0630

    Search by Name

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
             .filter((item) => {
               return search.toLowerCase() === ""
                 ? item
-                : item.website.toLowerCase().includes(search);
+                : item.name.toLowerCase().includes(search);
             })
             .map((item) => (
               <tr key={item.id}>
